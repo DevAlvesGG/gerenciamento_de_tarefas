cf51fe3b17198d036cdab62a503fe1039136e120
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-02-10 16:07:18 -0300

    feat: atiliza arquivo do servidor com novas rotas

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import { router } from "../routes";
+import { error } from "node:console";
+import { errorHandler } from "../middlewares/error-handler";
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api", router);
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta http://localhost:${PORT}`));
